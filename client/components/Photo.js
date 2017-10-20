3cfe62be7dd63d0188e275b2eeadfb04fc727893
JamesRiall <jamesrariall@gmail.com>
2017-10-20 08:54:32 +0100

    up to chapter 17

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -22,7 +22,7 @@ const Photo = React.createClass({
         <figcaption>
           <p>{post.caption}</p>
           <div className="control-buttons">
-            <button className="likes">&hearts; {post.likes}</button>
+            <button className="likes" onClick={this.props.increment.bind(null, i)}>&hearts; {post.likes}</button>
           </div>
           <Link className="button" to={`/view/${post.code}`}>
             <span className="comment-count">
